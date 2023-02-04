53a7958a2006ab8646084a16ca38ee5d9d9fae79
Hapsatousow <123075435+Hapsatousow@users.noreply.github.com>
2023-02-05 00:04:22 +0100

    NGROK

--- Jenkins_git.java
+++ Jenkins_git.java
@@ -1,5 +1,5 @@
 public class Jenkins_git {
     public static void main(String[] args) {
-        System.out.println("Bonjour Hapsatou Abou Sow ravi de te rencontré!");
+        System.out.println("Bonjour Hapsatou Abou Sow!");
     }
 }
