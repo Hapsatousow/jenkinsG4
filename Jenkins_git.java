ebc3aa698f7a74f53f0a76deae44417de55cb124
Hapsatousow <123075435+Hapsatousow@users.noreply.github.com>
2023-02-06 00:51:56 +0100

    dernier commit

--- Jenkins_git.java
+++ Jenkins_git.java
@@ -1,5 +1,5 @@
 public class Jenkins_git {
     public static void main(String[] args) {
-        System.out.println("Bonjour Hapsatou Abou Sow comment tu vas!");
+        System.out.println("Bonjour Hapsatou Abou Sow!");
     }
 }
